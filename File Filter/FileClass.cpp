efd6ad06ea6fbfc6acf6317d4650c92d3e8b7057
Charile1 <danleynemo1995@gmail.com>
2015-10-29 09:16:50 -0400

    Changed the the doFilter, now it doesn't post an extra character at the end.

--- File Filter/FileClass.cpp
+++ File Filter/FileClass.cpp
@@ -1,12 +1,14 @@
 
 #include "FileClass.h"
-
+#include <iostream>
+using namespace std;
 void FileClass::doFilter(ifstream &input, ofstream &output) {
 	char letter;
 
 	while(!input.eof())
 	{
 		input.get(letter);
+		if (!(input)) break;
 		output.put(transform(letter));
 	}
 }
